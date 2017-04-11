b6c2cb5a3a01b0ab1436ad2d4056ef4ce9a97d1e
John Gruska <johngruska@gmail.com>
2017-04-11 02:27:23 -0400

    Added board.js code

--- prototype/board.js
+++ prototype/board.js
@@ -1,1 +1,60 @@
-board.js
+var Deck = require('./deck.js');
+
+var Board = function(playerCount) {
+	this.playerCount = playerCount;
+	this.reset();
+};
+
+module.exports = Board;
+
+Board.prototype.reset = function() {
+	this.nobles = require('./noble.js');
+	this.decks = [new Deck(0), new Deck(1), new Deck(2)];
+	this.chips = this.getStartingChips();
+};
+
+Board.prototype.getStartingChips = function() {
+	var startingChipCounts = [0,0,4,5,7];
+	var startingChips = (5<<15);
+
+	for(var colorIndex = 0; colorIndex < 5; colorIndex++){
+		startingChips += (startingChipCounts[this.playerCount]<<(3*colorIndex));
+	}
+	return startingChips;
+};
+
+Board.prototype.getExposedCards = function() {
+	var exposedCards = [];
+
+	this.decks.forEach(function(deck){
+		for(var i = 0; i < Math.min(4, deck.cards.length); i++){
+			exposedCards.push(deck.cards[deck.cards.length-i-1]);
+		}
+	});
+	return exposedCards;
+};
+
+Board.prototype.getTopCards = function() {
+	var topCards = [];
+
+	this.decks.forEach(function(deck){
+		if(deck.cards.length > 4) {
+			topCards.push(deck.cards[deck.cards.length-6]);
+		} else {
+			topCards.push(null);
+		}
+	});
+	return exposedCards;
+};
+
+Board.prototype.removeCard = function(row,index) {
+	return decks[row].cards.splice(index,1);
+};
+
+Board.prototype.addChips = function(chips) {
+	this.chips += chips;
+};
+
+Board.prototype.removeChips = function(chips) {
+	this.chips -= chips;
+};
--- prototype/moves.js
+++ prototype/moves.js
@@ -2,9 +2,8 @@ const util = require('util')
 var chips = require('./chipMoves.js');
 var translateChipCount = chips.translateChipCount;
 var getChipTakingOptions = chips.getChipTakingOptions;
-var Deck = require('./deck.js');
-var nobles = require('./noble.js');
 var common = require('./common.js');
+var Board = require('./board.js');
 var Player = require('./player.js');
 
 if (process.argv.length < 3) {
@@ -14,31 +13,7 @@ if (process.argv.length < 3) {
 var center = Number(process.argv[2]); //23404;
 var player = Number(process.argv[3]); //9362;
 var chipTakingOptions = getChipTakingOptions(center, player);
-var decks = [new Deck(0), new Deck(1), new Deck(2)];
 var players = [new Player(0,"Player A"), new Player(1,"Player B")];
+var board = new Board(2);
 
-// console.log(util.inspect(decks, false, null));
-// console.log("Center chips:", common.translateChipCount(center));
-// console.log("Player chips:", common.translateChipCount(player));
-// console.log("Chip Moves  :", chipTakingOptions);
-// console.log("Nobles      :", nobles.map(common.translateChipCount));
-
-var ctr = 0;
-var lastCount = 0;
-var gold = 5;
-while(ctr++ < 500 && players[0].purchasedCards.length < 3){
-	if(players[0].reservedCards.length){
-		players[0].activateCard(players[0].reservedCards[0]);
-		console.log("XXXXXXX");	
-	}
-	console.log(players[0]);
-	players[0].addChips(1<<(3*Math.floor(Math.random()*5)));
-	players[0].buyCard(decks[0].cards[0]);
-	if(players[0].purchasedCards.length > lastCount){
-		console.log("*******");
-		decks[0].cards.shift();
-		players[0].reserveCard(decks[0].cards.pop(), gold--);
-	}
-	lastCount = players[0].purchasedCards.length;
-	console.log(players[0]);
-}
+console.log(board);
