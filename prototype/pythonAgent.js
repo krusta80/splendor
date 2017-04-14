655391d1f6dbd536322de86b969fe1834f4eb78d
John Gruska <johngruska@gmail.com>
2017-04-14 09:32:30 -0400

    Added sleep/timeout logic to python agent and verified low CPU usage

--- prototype/pythonAgent.js
+++ prototype/pythonAgent.js
@@ -2,11 +2,14 @@ var zerorpc = require("zerorpc");
 var Agent = require('./agent.js');
 var Game = require('./game.js');
 var show = require('./common.js').show;
+var sleep = require('sleep');
 
 var server = new zerorpc.Server({
     newSession: function(name, numberOfOpponents, reply) {
         this.name = name.toString();
+        this.nextResponse = null;
         var agents = [this];
+        console.log(show(this));
         for(var i = 0; i < numberOfOpponents; i++){
         	agents.push(new Agent("Opponent "+(i+1)));
         }
@@ -14,12 +17,18 @@ var server = new zerorpc.Server({
         reply(null, "Hello, " + name + show(this.game.players));
         console.log(show(this.game));
     },
-    newGame: function() {
+    newGame: function(reply) {
     	this.game.reset();
-    	this.game.playUntilEnd();
+    	reply(null, "New game: " + show(this.game));
+    	setTimeout(this.game.playUntilEnd.bind(this.game), 0);
     },
     makeMove: function(board, players, playerIndex, moves) {
     	//	TODO(jgruska): Need to make game wait for agent to move...
+    	console.log("Python make move!!");
+    	var timeout = 0;
+    	while(timeout++ < 1000 && !this.nextResponse) {
+    		sleep.msleep(10);
+    	}
     }
 });
 
